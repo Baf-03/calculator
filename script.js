f8f6dceb8a4d66fffd5c11406628708a3994c413
Bilal Ahmed <123253713+Baf-03@users.noreply.github.com>
2023-04-09 02:51:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+let string ="";
+let number_of_times_clicked =0;
+let buttons = document.querySelectorAll('.button');//yae sare buttons ko select kr lae ga jin ke class button hoge
+let d1=document.getElementById("display1");
+let d2=document.getElementById("display2");
+//abb mae iss ke array bana don ga
+
+let array_buttons=Array.from(buttons);
+// Array.from is a method in JavaScript
+ // that creates a new array from an array-like or iterable object.
+  // The method returns an array that contains all the elements of the input object, in the same order as they appear in the original object.
+// Here's an example usage of Array.from:
+// const str = "hello";
+// const arr = Array.from(str);
+// console.log(arr); // Output: ["h", "e", "l", "l", "o"]
+
+let x=function(e){
+	
+	if(e.target.innerHTML=="="){
+		
+		string=eval(string);
+		d2.innerHTML = `<div id="display2"><p>${string}</p></div>`;
+		let new_element = string.toString();
+		number_of_times_clicked=new_element.length;
+		console.log("it is"+number_of_times_clicked);
+
+		//if user press = button it will get inside this condition
+
+	}
+
+	else if(e.target.innerHTML=="⌫"){
+		d1.innerHTML = display1.innerHTML.slice(0, -1);
+		string=d1.innerHTML;	//if user press AC button it will get inside this condition
+		if(number_times_clicked<12){
+			number_times_clicked++
+		};
+	}
+	else if(e.target.innerHTML=="AC"){
+		d1.innerHTML = "";
+		d2.innerHTML = d1.innerHTML;
+		string=d1.innerHTML;	//if user press backspace it will get inside this condition
+		number_of_times_clicked=0;
+	}
+	else{
+		 //if user press numbers it will get inside this condition
+			if(number_of_times_clicked!=12){
+				string+=e.target.innerHTML;
+				d1.innerHTML = string;
+				number_of_times_clicked++
+				}
+		}
+
+}
+
+
+
+for(let i of array_buttons){
+	i.addEventListener('click',x);
+	
+
+}
+
